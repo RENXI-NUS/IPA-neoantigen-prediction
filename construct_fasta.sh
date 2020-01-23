dbb570f4fbcaeb090dbb261f201ec67c60c1d2a0
RENXI-NUS <58414732+RENXI-NUS@users.noreply.github.com>
2020-01-23 19:58:53 +0800

    Update construct_fasta.sh

--- construct_fasta.sh
+++ construct_fasta.sh
@@ -4,7 +4,6 @@ file=$1
 cancer=$2
 path=$3
 cd ${path}
-#cd /data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/intron_polyadenylated_peptide_for_MS/${cancer}_final_loose_criteria2_new1
 for line in $(cat "${file}" | awk '{print $(NF-2)}' ); do 
 #for line in $(cat "${file}" | cut -f5); do 
 	echo -n ">" >> ${file}.fa
