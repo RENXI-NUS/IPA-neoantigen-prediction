ac70145e0e7a09fc47f39893ec424f4191df5da8
RENXI-NUS <58414732+RENXI-NUS@users.noreply.github.com>
2020-03-05 17:59:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,16 @@ set -u
 bash configure.sh
 
 pname=$(basename $0)
+
+$result_file="$1/$3"
+mkdir -p "$result_file"
+
 [ $# -ne 5 ] && { printf "Prediction of neoantigens derived from intronic polyadenylation.
 Usage: $pname [full path for the prediction] [peptide length] [cancer name] [full path for the bam file] [output path]
 
 [current full path]		STRING without space; use _ for space.Tab file only.
 [peptide length]		The length of peptide to be used for binding affinity prediction. 
-[cancer name]		The cancer name for prediction. Please use TCGA abbreviation, e.g. BLCA BRCA COAD GBM LAML LIHC LUAD LUSC OV PRAD SKCM STAD
+[dataset name]		The dataset name for prediction. Please use TCGA abbreviation, e.g. BLCA BRCA COAD GBM LAML LIHC LUAD LUSC OV PRAD SKCM STAD
 [full path for the bam files]		The corresponding full path for the unsorted bam files (i.e. sort by name) of the cancers. STRING without space; use _ for space.Tab file only
 [outputfile path]		STRING without space; use _ for space.Tab file only.\n
 "; exit 1; }
@@ -27,5 +31,5 @@ fi
 cd $1
 
 bash "$1"/predict_polyA_spanning_reads.sh "$1" "$2" "$3" "$4" "$5" > log.out 
+bash "$1"/run_Seq2HLA.sh "$1" "$2" "$3" "$4" "$5" > log.out 
 bash "$1"/run_neoepitope_pipeline.sh "$1" "$2" "$3" "$4" "$5" > log.out
-
