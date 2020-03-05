101c80d441c293ae874a7311cb15cd8af40c971f
RENXI-NUS <58414732+RENXI-NUS@users.noreply.github.com>
2020-03-05 21:10:17 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 ##provide full path of current directory (STRING without space; use _ for space):
-$1=/data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/scripts/test/IPA-neoantigen-prediction
+curr_dir=/data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/scripts/test/IPA-neoantigen-prediction
 
 ##provide peptide length (The length of peptide to be used for binding affinity prediction, and must be from 8 to 14 amino acids)
-$2=9
+peptide_length=9
 
 ##dataset name (the dataset name of this prediction):
-$3=test
+dataset_name=test
 
 ##full path for the bam files and raw fastq files (The corresponding full path for the unsorted bam files (i.e. sort by name) and raw fastq files of the analyzed dataset. STRING without space; use _ for space):
-$4=/data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/scripts/test/IPA-neoantigen-prediction
+bam_dir=/data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/scripts/test/IPA-neoantigen-prediction
 
 ##outputfile path (STRING without space):
-$5=/data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/scripts/test/IPA-neoantigen-prediction
+output_dir=/data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/scripts/test/IPA-neoantigen-prediction
