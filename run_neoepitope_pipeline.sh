5a53988eac58964bdab422d274d42be329f44d69
RENXI-NUS <58414732+RENXI-NUS@users.noreply.github.com>
2020-03-06 11:41:16 +0800

    Update run_neoepitope_pipeline.sh

--- run_neoepitope_pipeline.sh
+++ run_neoepitope_pipeline.sh
@@ -7,13 +7,13 @@ bam_path=$4
 file_path=$5
 
 cd ${path}
-cp findPeak.cluster.pl splicing_coordinates_TCGA_12_cancers1 EncodeGencode_merged_intron_selected_excluded_exon_- EncodeGencode_merged_intron_selected_excluded_exon_+ callCluster.pl gencode_PASs_extended_by_100_filtered hg19_intron.excluded.exon.bed PASRA2PeptideSeqs.py ./${file_path}
+cp findPeak.cluster.pl splicing_coordinates_TCGA_12_cancers1 EncodeGencode_merged_intron_selected_excluded_exon_- EncodeGencode_merged_intron_selected_excluded_exon_+ callCluster.pl gencode_PASs_extended_by_100_filtered hg19_intron.excluded.exon.bed PASRA2PeptideSeqs.py ${file_path}
 cd ${file_path}
 python=/data/11000039/e0149673/scratch/bin/anaconda2/envs/py2/bin/python
 netMHCpan=/data/11000039/e0149673/scratch/bin/software/netMHCpan-4.0/netMHCpan
 
 ## identify intronic PASs 
-files="${file_path}/${cancer}.list"
+files="${cancer}.list"
 for file in $(cat ${files}); do awk '$4 !~ /N/' ${file}_potential_polyA_sites_from_softclipped_reads.txt >> ${file}.0; done ##remove splicing reads
 for file in $(cat ${files}); do awk 'NR==FNR{a[$2]++; next} a[$2]>=2' <(sort -k3,3r -k1,1 -k2,2n ${file}.0) <(sort -k3,3r -k1,1 -k2,2n ${file}.0) > ${file}.0.1; done	# print the PASs that supported by at least two soft-clipping reads
 for file in $(cat ${files}); do sort -k3,3r -k1,1 -k2,2n ${file}.0.1 | awk '{if ($3 == "+") print $1"\t"$2"\t"$2+1"\t"$4"\t0\t"$3}' > ${file}.0.1.output; done
