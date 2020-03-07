665934cb0bb8d679a4e60ed7e625684f01b660d7
RENXI-NUS <58414732+RENXI-NUS@users.noreply.github.com>
2020-03-07 15:35:15 +0800

    Update construct_fasta.sh

--- construct_fasta.sh
+++ construct_fasta.sh
@@ -4,8 +4,7 @@ file=$1
 cancer=$2
 path=$3
 cd ${path}
-for line in $(cat "${file}" | awk '{print $(NF-2)}' ); do 
-#for line in $(cat "${file}" | cut -f5); do 
+for line in $(cat "${file}" | cut -f5); do 
 	echo -n ">" >> ${file}.fa
 	echo "${line}" >> ${file}.fa
 	echo "${line}" >> ${file}.fa
