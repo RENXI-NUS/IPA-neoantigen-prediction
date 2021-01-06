85f8dc44da27b4f0429e6a803daf29430bfd3bf4
RENXI-NUS <58414732+RENXI-NUS@users.noreply.github.com>
2021-01-06 11:27:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /data/11000039/e0149673/scratch/Projects/TCGA_unsorted_bam/scripts/test/IPA-neoantigen-prediction
-curr_dir=`grep curr_dir run.config | sed s/.*\=//`
-peptide_length=`grep peptide_length run.config | sed s/.*\=//`
-dataset_name=`grep dataset_name run.config | sed s/.*\=//`
-bam_dir=`grep bam_dir run.config | sed s/.*\=//`
-output_dir=`grep output_dir run.config | sed s/.*\=//`
+installDIR=`grep installDIR run.configure | sed s/.*\=//`
+peptide_length=`grep peptide_length run.configure | sed s/.*\=//`
+dataset_name=`grep dataset_name run.configure | sed s/.*\=//`
+bam_dir=`grep bam_dir run.configure | sed s/.*\=//`
+output_dir=`grep output_dir run.configure | sed s/.*\=//`
 
 mkdir -p "${output_dir}"
 mkdir -p "${output_dir}/${dataset_name}"
@@ -14,13 +14,13 @@ mkdir -p "$logs"
 cd ${bam_dir}
 ls *"bam" > "${output_dir}/${dataset_name}.list"
 
-cd $curr_dir
+cd $installDIR
 
 ## run HLA typing with Seq2HLA
-bash "$curr_dir"/run_Seq2HLA.sh "$curr_dir" "$peptide_length" "$dataset_name" "$bam_dir" "$output_dir" > log.out 
+bash "$installDIR"/run_Seq2HLA.sh "$installDIR" "$peptide_length" "$dataset_name" "$bam_dir" "$output_dir" > log.out 
 
 ## detect soft-clipped reads for downstream IPA analysis
-bash "$curr_dir"/predict_polyA_spanning_reads.sh "$curr_dir" "$peptide_length" "$dataset_name" "$bam_dir" "$output_dir" > log.out 
+bash "$installDIR"/predict_polyA_spanning_reads.sh "$installDIR" "$peptide_length" "$dataset_name" "$bam_dir" "$output_dir" > log.out 
 
 ## predict potential IPA neoantigens (configure the run.configure file firstly)
-bash "$curr_dir"/run_neoepitope_pipeline.sh > log.out
+bash "$installDIR"/run_neoepitope_pipeline.sh > log.out
